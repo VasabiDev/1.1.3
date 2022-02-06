db05336570c017a44d3d258e261906de6713e77b
Anastasia Konovalova <pochtaplay2013@gmail.com>
2022-02-06 21:59:34 +0300

    UserServiceImpl fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,39 +9,33 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-   // private final UserDao userDaoJDBC = new UserDaoJDBCImpl();
+
     private final UserDao userDaoJDBCHibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
         userDaoJDBCHibernate.createUsersTable();
-        //userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-
         userDaoJDBCHibernate.dropUsersTable();
-        //userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBCHibernate.saveUser(name, lastName, age);
-        //userDaoJDBC.saveUser(name, lastName, age);
+
     }
 
     public void removeUserById(long id) {
         userDaoJDBCHibernate.removeUserById(id);
-        //userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
         return userDaoJDBCHibernate.getAllUsers();
 
-        //return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
         userDaoJDBCHibernate.cleanUsersTable();
-        //userDaoJDBC.cleanUsersTable();
     }
 
 
